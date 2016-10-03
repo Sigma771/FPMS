2ba6ab0e19748f0ffbca6531e657707329875dff
Jolan Barron <JBarron@mslc.com>
2016-10-03 16:46:10 -0400

    Tables are now populating with test data, some back end setup for the forms has been started, front end for edit, and adding pages has been tossed in.

--- scripts/controllers/doctorcontroller.js
+++ scripts/controllers/doctorcontroller.js
@@ -1,19 +1,60 @@
 var mainApp = angular.module("mainApp", []);
+var doctorsArray = {"doctors":[{"firstName":"Stephen", "lastName":"Strange", "phoneNumber":"317-987-6543", "address":"7586 West Main St", "city":"Gotham", "state":"NY", "zip":"11417", "type":"Family"},
+                                {"firstName":"Hank", "lastName":"Pym", "phoneNumber":"219-487-8888", "address":"7586 Column Ave", "city":"Louisville", "state":"KY", "zip":"40220", "type":"Family"},
+                                {"firstName":"Nathaniel", "lastName":"Essex", "phoneNumber":"219-487-8888", "address":"3245 Mystery St", "city":"Chicago", "state":"IL", "zip":"60608", "type":"Family"}]};
+var doctorInfo = {firstName: "", lastName: "", phoneNumber: "", type: "", address: "", city: "",
+                     state: "", zip: ""};
 
 mainApp.controller('doctorController', function($scope){
-prepareLocalStorageObject();
+    prepareLocalStorageObject();
+
+    $scope.doctorsList = getAllDoctors();
+});
+
+mainApp.controller('newDoctorController', function($scope){
+    $scope.master = {firstName: "", lastName: "", phoneNumber: "", type: "", address: "", city: "",
+                     state: "", zip: ""};
+
+    $scope.form.firstName = function(){
+        
+    };
+});
+
+mainApp.controller('editDoctorController', function($scope){
+    $scope.master = {firstName: doctorInfo.firstName, lastName: doctorInfo.lastName, phoneNumber: doctorInfo.phoneNumber, type: doctorInfo.type, address: doctorInfo.address, city: doctorInfo.city,
+                     state: doctorInfo.state, zip: doctorInfo.zip};
+    $scope.doctor = angular.copy($scope.master);
 });
 
 function prepareLocalStorageObject(){
-//alert(localStorage.doctors);
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        if(localStorage.doctors == undefined){
+            localStorage.doctors = JSON.stringify(doctorsArray);
+        }
+        else{
+            doctorsArray = JSON.parse(localStorage.doctors);
+        }
+    } else {
+        // Sorry! No Web Storage support..
+        alert('Your browser does not support local storage. The minimum version requirement for browsers that support this techonology are as follows: ' +
+              'Chrome v4.0 -- Internet Explorer v8.0 -- Firefox v 3.5 -- Safari v4.0 -- Opera v11.5');
+    }
 }
     
 function getAllDoctors(){
-
+    return doctorsArray;
 }
 
-function getDoctor(){
-
+function getDoctor(doctor){
+    doctorInfo.firstName = doctor.firstName;
+    doctorInfo.lastName = doctor.lastName;
+    doctorInfo.type = doctor.type;
+    doctorInfo.phoneNumber = doctor.phoneNumber;
+    doctorInfo.address = doctor.address;
+    doctorInfo.city = doctor.city;
+    doctorInfo.state = doctor.state;
+    doctorInfo.zip = doctor.zip;
 }
 
 function saveDoctor(){
--- scripts/controllers/patientcontroller.js
+++ scripts/controllers/patientcontroller.js
@@ -1,21 +1,73 @@
 var mainApp = angular.module("mainApp", []);
+var patientsArray = {"patients":[{"firstName":"Bruce", "lastName":"Wayne", "ssn":"111-11-1111", "phoneNumber":"317-123-4567", "gender":"M", "address":"1234 Wayne Manor", "city":"Gotham", 
+                                  "state":"NY", "zip":"11417", "InsuranceProvider":"Blue Cross Blue Shield", "insuranceProviderNumber":"PN1234567", "physician":"Stephen Strange"},
+                                 {"firstName":"John", "lastName":"Smith", "ssn":"222-22-2222", "phoneNumber":"317-234-5678", "gender":"NB", "address":"4321 Main St", "city":"Louisville", 
+                                  "state":"KY", "zip":"40220", "InsuranceProvider":"United Health Network", "insuranceProviderNumber":"PN4839483", "physician":"Stephen Strange"},
+                                 {"firstName":"Joe", "lastName":"Somebody", "ssn":"333-33-3333", "phoneNumber":"317-345-6789", "gender":"M", "address":"8947 Wesley Ave", "city":"Chicago", 
+                                  "state":"IL", "zip":"60608", "InsuranceProvider":"Blue Cross Blue Shield", "insuranceProviderNumber":"PN4839573", "physician":"Stephen Strange"}]};
+var patientInfo = {firstName: "", lastName: "", ssn: "", phoneNumber: "", gender: "", address: "", city: "",
+                     state: "", zip: "", InsuranceProvider: "", insuranceProviderNumber: "", physician: ""};
 
 mainApp.controller('patientController', function($scope){
+    prepareLocalStorageObject();
 
+    $scope.patientsList = getAllPatients();
+});
+
+mainApp.controller('newPatientController', function($scope){
+    $scope.master = {firstName: "", lastName: "", ssn: "", phoneNumber: "", gender: "", address: "", city: "",
+                     state: "", zip: "", InsuranceProvider: "", insuranceProviderNumber: "", physician: ""};
+    $scope.patient = angular.copy($scope.master);
+
+    $scope.form.firstName = function(){
+        
+    };
+    $scope.form.doctorsList = function(){
+        return JSON.parse(localStorage.doctors);
+    };
+
+    $scope.savePatient = function() {
+        
+    };
+});
+
+mainApp.controller('editPatientController', function($scope){
+    $scope.master = {firstName: patientInfo.firstName, lastName: patientInfo.lastName, ssn: patientInfo.ssn, phoneNumber: patientInfo.phoneNumber, gender: patientInfo.gender, address: patientInfo.address, city: patientInfo.city,
+                     state: patientInfo.state, zip: patientInfo.zip, InsuranceProvider: patientInfo.InsuranceProvider, insuranceProviderNumber: patientInfo.insuranceProviderNumber, physician: patientInfo.physician};
+    $scope.patient = angular.copy($scope.master);
 });
 
 function prepareLocalStorageObject(){
-
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        if(localStorage.patients == undefined){
+            localStorage.patients = JSON.stringify(patientsArray);
+        }
+        else{
+            patientsArray = JSON.parse(localStorage.patients);
+        }
+    } else {
+        // Sorry! No Web Storage support..
+        alert('Your browser does not support local storage. The minimum version requirement for browsers that support this techonology are as follows: ' +
+              'Chrome v4.0 -- Internet Explorer v8.0 -- Firefox v 3.5 -- Safari v4.0 -- Opera v11.5');
+    }
 }
 
 function getAllPatients(){
-
+    return patientsArray;
 }
 
-function getPatient(){
-
-}
-
-function savePatient(){
-
+function getPatient(patient){
+    patientInfo.firstName = patient.firstName;
+    patientInfo.lastName = patient.lastName;
+    patientInfo.ssn = patient.ssn;
+    patientInfo.phoneNumber = patient.phoneNumber;
+    patientInfo.gender = patient.gender;
+    patientInfo.address = patient.address;
+    patientInfo.city = patient.city;
+    patientInfo.state = patient.state;
+    patientInfo.zip = patient.zip;
+    patientInfo.InsuranceProvider = patient.InsuranceProvider;
+    patientInfo.insuranceProviderNumber = patient.insuranceProviderNumber;
+    patientInfo.physician = patient.physician;
 }
